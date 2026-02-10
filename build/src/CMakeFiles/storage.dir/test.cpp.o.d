f55e8f4fabe2a26d61fbfcd313e08ab07e7eaf0e
Raul Albín <raul200794@gmail.com>
2026-02-10 23:06:32 +0100

    remove build directory from git

--- build/src/CMakeFiles/storage.dir/test.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/storage.dir/test.cpp.o: \
-  /Users/raulalbin/Desktop/Desarrollo/cpp/almacen/src/test.cpp \
-  /Users/raulalbin/Desktop/Desarrollo/cpp/almacen/src/../include/test.h
--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/raulalbin/Desktop/Desarrollo/cpp/almacen/test
-# Build directory: /Users/raulalbin/Desktop/Desarrollo/cpp/almacen/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/raulalbin/Desktop/Desarrollo/cpp/almacen/build/test/UnitTests[1]_include.cmake")
