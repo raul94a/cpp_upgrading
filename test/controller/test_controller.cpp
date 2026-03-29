1ac0464471ce6308164b88f2bc4c9d29f4d4976d
Raul Albín <raul200794@gmail.com>
2026-03-29 22:42:36 +0200

    improve WriteToLocalFile test

--- test/controller/test_controller.cpp
+++ test/controller/test_controller.cpp
@@ -14,16 +14,29 @@ TEST(StorageControllerTest, ReadFromLocalFile) {
 }
 
 TEST(StorageControllerTest, WriteToLocalFile) {
-    StorageController cntr = StorageController(FilePersister());
-    StoragedProduct product = StoragedProduct(1,"Camiseta de rayas", 99.99, 1);
-    Storage st = Storage(1,"Almacenes Llorones", "Martos");
+    FilePersister persister;
+    StorageController cntr(persister);
+    
+    StoragedProduct product(1, "Camiseta de rayas", 99.99, 1);
+    Storage st(1, "Almacenes Llorones", "Martos");
     st.add_product(&product);
-    std::vector<Storage> storages = cntr.get_all();
+    
     cntr.add_storage(st);
-    std::cout << "Number of storages: " << cntr.get_all().size() << std::endl;
+    ASSERT_EQ(cntr.get_all().size(), 1); // Verificación previa
     cntr.save_storages();
-    cntr.get_all().clear();
-    EXPECT_NE(cntr.read_storages().size(),2);
-    EXPECT_EQ(cntr.read_storages().size(),1);
+
+    
+    StorageController cntr2(persister); 
+    std::vector<Storage> loaded_storages = cntr2.read_storages();
+
+    EXPECT_EQ(loaded_storages.size(), 1);
+    
+    if (!loaded_storages.empty()) {
+        EXPECT_EQ(loaded_storages[0].name, "Almacenes Llorones");
+        EXPECT_EQ(loaded_storages[0].city, "Martos");
+        
+        
+        EXPECT_EQ(loaded_storages[0].products.size(), 1);
+    }
 
 }
